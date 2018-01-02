3dcca28d1dfb717662a808a600046420ed4cdf94
Marek Pekarovič <marek.pekarovic@gmail.com>
2018-01-02 14:44:11 +0100

    added some declarations

--- Ping-Pong/Ball.h
+++ Ping-Pong/Ball.h
@@ -1,1 +1,12 @@
 #pragma once
+
+class Ball
+{
+public:
+	void DrawBall();
+private:
+	int BallRadius;
+	int BallVell;
+	int BallPos;
+	int BallSpeed;
+};
--- Ping-Pong/Paddle.h
+++ Ping-Pong/Paddle.h
@@ -1,1 +1,11 @@
 #pragma once
+
+class Paddle
+{
+public:
+	void DrawPaddle();
+private:
+	int PadWidth;
+	int PadHeight;
+	int PaddleSpeed;
+};
